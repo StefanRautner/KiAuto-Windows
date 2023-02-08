8d092e78746eaa5e8a0f661d5eaffb5107987dbf
Salvador E. Tropea <salvador@inti.gob.ar>
2023-02-08 14:18:58 -0300

    Disabling CPU usage detection for KiCad 7, is failing

--- tests/pcbnew/test_3d.py
+++ tests/pcbnew/test_3d.py
@@ -27,7 +27,7 @@ PROG = 'pcbnew_do'
 def test_3d_view_1(test_dir):
     """ Simple 3D Viewer test """
     ctx = context.TestContext(test_dir, '3DView_1', 'good-project')
-    cmd = [PROG, '-vvv', '3d_view', '--zoom', '3', '-x', '1', '--output_name', 'good_3d_rt_1.png', '-r', '-d', '-w', '600',
+    cmd = [PROG, '-vvv', '3d_view', '--zoom', '3', '-x', '1', '--output_name', 'good_3d_rt_1.png', '-r', '-d', '-w', '20',
            '-X', '3', '-Z', '3']
     ctx.run(cmd)
     ctx.compare_image('good_3d_rt_1.png', fuzz='50%')
@@ -38,7 +38,7 @@ def test_3d_view_2(test_dir):
     """ Simple 3D Viewer test """
     ctx = context.TestContext(test_dir, '3DView_2', 'good-project')
     cmd = [PROG, '-vvv', '3d_view', '--zoom', '6', '-x', '-1', '--output_name', 'good_3d_rt_2.png', '--no_smd', '-r', '-O',
-           '-d', '-w', '600']
+           '-d', '-w', '20']
     ctx.run(cmd)
     ctx.compare_image('good_3d_rt_2.png', fuzz='50%')
     ctx.clean_up()
