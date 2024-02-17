d1893edaaebe3d8127490d42c804f1a80cfc3198
Salvador E. Tropea <salvador@inti.gob.ar>
2024-02-17 14:39:13 -0300

    [KiCad 8][3D Render] Adapted tests
    - Also removed the layer manager panel

--- tests/pcbnew/test_3d.py
+++ tests/pcbnew/test_3d.py
@@ -27,8 +27,11 @@ PROG = 'pcbnew_do'
 def test_3d_view_1(test_dir):
     """ Simple 3D Viewer test """
     ctx = context.TestContext(test_dir, '3DView_1', 'good-project')
-    cmd = [PROG, '-vvv', '3d_view', '--zoom', '3', '-x', '1', '--output_name', 'good_3d_rt_1.png', '-r', '-d', '-w', '20',
+    cmd = [PROG, '-r', '-vvv', '3d_view', '-x', '1', '--output_name', 'good_3d_rt_1.png', '-r', '-d', '-w', '20',
            '-X', '3', '-Z', '3']
+    if not context.ki8:
+       # KiCad 8 does a zoom to fit
+       cmd.extend(['--zoom', '3'])
     ctx.run(cmd)
     ctx.compare_image('good_3d_rt_1.png', fuzz='50%', tol=2000)
     ctx.clean_up()
@@ -37,8 +40,10 @@ def test_3d_view_1(test_dir):
 def test_3d_view_2(test_dir):
     """ Simple 3D Viewer test """
     ctx = context.TestContext(test_dir, '3DView_2', 'good-project')
-    cmd = [PROG, '-vvv', '3d_view', '--zoom', '6', '-x', '-1', '--output_name', 'good_3d_rt_2.png', '--no_smd', '-r', '-O',
-           '-d', '-w', '20']
+    cmd = [PROG, '-vvv', '3d_view', '-x', '-1', '--output_name', 'good_3d_rt_2.png', '--no_smd', '-r', '-O',
+           '-d', '-w', '20', '--zoom']
+    # KiCad 8 does a zoom to fit
+    cmd.append('1' if context.ki8 else '6')
     ctx.run(cmd)
     ctx.compare_image('good_3d_rt_2.png', fuzz='50%', tol=2000)
     ctx.clean_up()
